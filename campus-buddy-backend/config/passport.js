09ab35eba6004d94140544d902b4f71dfe244109
bandlapraharshitha <praharshitha35@gmail.com>
2025-08-28 15:15:28 +0530

    edited backend folder

--- campus-buddy-backend/config/passport.js
+++ campus-buddy-backend/config/passport.js
@@ -4,7 +4,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
   clientID: '557622434609-3poo5ik8acmlj0u59hi31v2ugvuukfaa.apps.googleusercontent.com',
   clientSecret: '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',
-  callbackURL: '/auth/google/callback'
+  callbackURL: 'https://campus-buddy-backend.onrender.com/auth/google/callback'
 },
 (accessToken, refreshToken, profile, done) => {
   const email = profile.emails[0].value;
